76f7e617d59890a8afe7573026bedab2354d9a2b
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-02-10 18:31:55 +0300

    Adding a re-usable Button component and implementing form validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import React from "react";
+import AddUser from "./Users/AddUser";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
---
+++ src/Users/AddUser.js
@@ -0,0 +1,66 @@
+import React, {useState} from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    if (
+      enteredUsername.trim().length === 0 ||
+      +enteredAge.trim().length === 0
+    ) {
+      return console.log("Invalid input");
+    }
+
+    if (+enteredAge < 1) {
+      return console.log("Please input age > 0");
+    }
+
+    const userData = {
+      username: enteredUsername,
+      age: +enteredAge,
+    };
+
+    console.log(userData);
+
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
