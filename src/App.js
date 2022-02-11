7bd6f3310b589674ec3a8b765020b74409b73994
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-02-11 15:03:53 +0300

    State lifting with adding new user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
-import AddUser from "./Users/AddUser";
+import React, { useState } from 'react';
+import AddUser from './Users/AddUser';
+import UsersList from './Users/UsersList';
 
 function App() {
-  return (
-    <div>
-      <AddUser />
-    </div>
-  );
+	const [usersList, setUsersList] = useState([]);
+	// usersList = [
+	// 	{ username: 'Vlad', age: 26 },
+	// 	{ username: 'Nastassia', age: 24 },
+	// ];
+	const addUserHandler = (uName, uAge) => {
+		setUsersList(() => [
+			...usersList,
+			{ username: uName, age: uAge, id: Math.random().toString() },
+		]);
+	};
+
+	return (
+		<div>
+			<AddUser onAddUser={addUserHandler} />
+			<UsersList users={usersList} />
+		</div>
+	);
 }
 
 export default App;
